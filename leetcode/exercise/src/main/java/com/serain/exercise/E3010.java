1449d38d5ef952e6d7ea089f8198be6aded3d196
jianchuanZeng <jianchuanz@foxmail.com>
2026-02-06 19:42:53 +0800

    2026/2/6

--- leetcode/exercise/src/main/java/com/serain/exercise/E3010.java
+++ leetcode/exercise/src/main/java/com/serain/exercise/E3010.java
@@ -1,14 +1,23 @@
 /**
  * @author Serain
- * @date  
+ * @date 
  */
 package com.serain.exercise;
 
 import java.util.Arrays;
 
 public class E3010 {
+    /**
+     * 计算数组的最小成本
+     * 规则：第一个元素必须选，然后从剩余元素中选两个最小的
+     * 
+     * @param nums 整数数组
+     * @return 最小成本
+     */
     public int minimumCost(int[] nums) {
+       // 从索引1开始排序数组，保持第一个元素不变
        Arrays.sort(nums, 1, nums.length);
+       // 返回第一个元素加上排序后前两个最小的元素
        return nums[0] + nums[1] + nums[2];
     }
 }
--- leetcode/exercise/src/main/java/com/serain/exercise/E3637.java
+++ leetcode/exercise/src/main/java/com/serain/exercise/E3637.java
@@ -1,28 +1,35 @@
 /**
  * @author Serain
- * @date  
+ * @date 
  */
 package com.serain.exercise;
 
 public class E3637 {
+    /**
+     * 判断数组是否为 Trionic 序列
+     * Trionic 序列定义：长度至少为 3 的序列，满足先严格递增，然后严格递减，最后再次严格递增
+     * 
+     * @param nums 输入整数数组
+     * @return 是否为 Trionic 序列
+     */
     public boolean isTrionic(int[] nums) {
         int n = nums.length;
         int i = 0;
         
-        // First segment: strictly increasing
+        // 第一段：严格递增
         while (i < n - 1 && nums[i] < nums[i + 1]) {
             i++;
         }
         if (i <= 0) return false;
         
-        // Second segment: strictly decreasing
+        // 第二段：严格递减
         int p = i;
         while (i < n - 1 && nums[i] > nums[i + 1]) {
             i++;
         }
         if (i <= p) return false;
         
-        // Third segment: strictly increasing
+        // 第三段：严格递增
         int q = i;
         while (i < n - 1 && nums[i] < nums[i + 1]) {
             i++;
